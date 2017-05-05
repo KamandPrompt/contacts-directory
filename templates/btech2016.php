adda07541871342b172e0faf9f3b84807c01e753
Abhigyan Khaund <mail@abhigyan.xyz>
2017-05-05 22:53:13 +0530

    add all contact directories

--- templates/btech2016.php
+++ templates/btech2016.php
@@ -1,7 +1,7 @@
 <DOCTYPE html>
-
+<html>
     <head>
-        <title> Contacts </title>
+        <title> Contacts - BTech 2016</title>
         <style>
         /* The side navigation menu */
         
@@ -215,36 +215,36 @@
 
     <body>
         <div id="mySidenav" class="sidenav">
-            <a href="insite.iitmandi.ac.in"><img src="../public/img/images.png" height="150" width="230" style="margin-left: -15px;" /></a>
+            <a href="index.html"><img src="../public/img/images.png" height="150" width="230" style="margin-left: -15px;" /></a>
             <div class="Dropdown">
                 <button class="dropbtn">B.Tech Batch</button>
                 <div class="Dropdown-content">
                     <a href="#">B.Tech 2016</a>
-                    <a href="#">B.Tech 2015</a>
-                    <a href="#">B.Tech 2014</a>
-                    <a href="#">B.Tech 2013</a>
+                    <a href="btech2015.php">B.Tech 2015</a>
+                    <a href="btech2014.php">B.Tech 2014</a>
+                    <a href="btech2013.php">B.Tech 2013</a>
                 </div>
             </div>
             <div class="Dropdown">
                 <button class="dropbtn">Post Graduate</button>
                 <div class="Dropdown-content">
-                    <a href="#">MS</a>
-                    <a href="#">M.Sc</a>
-                    <a href="#">M.Tech</a>
+                    <a href="ms.php">MS</a>
+                    <a href="msc.php">M.Sc</a>
+                    <a href="mtech.php">M.Tech</a>
                 </div>
             </div>
             <div class="Dropdown">
                 <button class="dropbtn">Ph.D</button>
                 <div class="Dropdown-content">
-                    <a href="#">Ph.D</a>
-                    <a href="#">Int Ph.D</a>
+                    <a href="phd.php">Ph.D</a>
+                    <a href="iphd.php">Int Ph.D</a>
                 </div>
             </div>
         </div>
         <div id="myPage" class="page">
             <h1>Indian Institute Of Technology Mandi</h1>
-            <h3>Contacts Directory</h3>
-            <input type="text" name="search" id="search" placeholder="Search Contact" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Contact'">
+            <h3>Contacts Directory - BTech 2016</h3>
+            <input type="text" name="search" id="searchb2016" placeholder="Search by Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search by Name'">
             <table>
                 <tr>
                     <th>Roll No.</th>
@@ -253,9 +253,37 @@
                     <th>Hostel</th>
                     <th>Room No.</th>
                     <th>Personal Contact</th>
-                    <th>Emergency Contact Name</th>
                     <th>Emergency Contact</th>
                 </tr>
+                <?php
+                  // connect to mongodb
+
+
+                try {
+
+                    $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+                    $options = [
+                        'sort' => ['roll' => 1],
+                    ];
+                    $query = new MongoDB\Driver\Query([],$options); 
+                    $rows = $mng->executeQuery("directory.btech2016", $query);
+                    foreach ($rows as $row) {
+                    
+                        echo "<tr class='details'><td>$row->roll</td><td>$row->name</td><td>$row->branch</td><td>$row->hostel</td><td>$row->room</td><td>$row->number</td><td>$row->emergency</td>" . "</tr>";
+                    }
+                    
+                } catch (MongoDB\Driver\Exception\Exception $e) {
+
+                    $filename = basename(__FILE__);
+                    
+                    echo "The $filename script has experienced an error.\n"; 
+                    echo "It failed with the following exception:\n";
+                    
+                    echo "Exception:", $e->getMessage(), "\n";
+                    echo "In file:", $e->getFile(), "\n";
+                    echo "On line:", $e->getLine(), "\n";       
+                } 
+                ?><!-- 
                 <tr>
                     <td>B16038</td>
                     <td>Sylvia</td>
@@ -265,37 +293,10 @@
                     <td>7807105329</td>
                     <td>Mr. Rajiv Mittal</td>
                     <td>9416220759</td>
-                </tr>
-                <tr>
-                    <td>B16082</td>
-                    <td>Abhigyan</td>
-                    <td>Mechanical Engineering</td>
-                    <td>B1 Parashar</td>
-                    <td>001</td>
-                    <td>??</td>
-                    <td>??</td>
-                    <td>780105329</td>
-                </tr>
-                <tr>
-                    <td>B16038</td>
-                    <td>Sylvia</td>
-                    <td>Computer Sceince and Engineering</td>
-                    <td>B4 Chandertaal</td>
-                    <td>001</td>
-                    <td>7807105329</td>
-                    <td>Mr. Rajiv Mittal</td>
-                    <td>9416220759</td>
-                </tr>
-                <tr>
-                    <td>B16038</td>
-                    <td>Sylvia</td>
-                    <td>Computer Sceince and Engineering</td>
-                    <td>B4 Chandertaal</td>
-                    <td>001</td>
-                    <td>7807105329</td>
-                    <td>Mr. Rajiv Mittal</td>
-                    <td>9416220759</td>
-                </tr>
+                </tr> -->
             </table>
         </div>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="../public/js/search.js"></script>
     </body>
+</html>
